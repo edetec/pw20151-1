f46aae6646428869064df0d86366ef3bf12cd8b8
bruno_schmitt <bruno_alexandre.s@hotmail.com>
2015-04-08 22:22:17 -0300

    mudança no nome do metodo inserirPessoa para inserir

--- IntroJPA/src/br/senai/sc/ti20132n1/pw/introjpa/Main.java
+++ IntroJPA/src/br/senai/sc/ti20132n1/pw/introjpa/Main.java
@@ -19,7 +19,7 @@ public class Main {
 				.createEntityManager();
 		entityManager.getTransaction().begin();
 
-		 inserirPessoa(entityManager);
+		 inserir(entityManager);
 		// buscarPorId(entityManager);
 		// excluir(entityManager);
 		// atualizar(entityManager);
@@ -71,7 +71,7 @@ public class Main {
 		System.out.println("ID:" + pessoa.getId());
 	}
 
-	private static void inserirPessoa(EntityManager entityManager) {
+	private static void inserir(EntityManager entityManager) {
 		Pessoa pessoa;
 		pessoa = new Pessoa();
 		// pessoa.setId(1L);
